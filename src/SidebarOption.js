86e448afd9968c1ea9729e871202d18baceb097c
Nicolas <nicobsas+webdev@gmail.com>
2021-03-13 02:41:47 +0900

    Add responsiveness

--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -2,7 +2,14 @@ import "./SidebarOption.css";
 
 const SidebarOption = ({ active, text, Icon }) => {
   return (
-    <div className={`sidebarOption ${active && "sidebarOption--active"}`}>
+    <div
+      className={`sidebarOption ${active && "sidebarOption--active"}`}
+      onClick={() =>
+        alert(
+          "This is a test app. Further functionality will be added in the future"
+        )
+      }
+    >
       <Icon />
       <h2>{text}</h2>
     </div>

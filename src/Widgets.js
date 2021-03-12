8b6726ccbe444b3abdaf7a70581ca69e6aeb8078
Nicolas <nicobsas+webdev@gmail.com>
2021-03-13 00:58:55 +0900

    Add widget section

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,9 +1,35 @@
 import "./Widgets.css";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Widgets = () => {
   return (
     <div className="widgets">
-      <h2>Widgets!</h2>
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input type="text" placeholder="Search Twitter" />
+      </div>
+
+      <div className="widgets__widgetContainer">
+        <h2>What's hapening</h2>
+
+        <TwitterTweetEmbed tweetId={"1100831364042162176"} />
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="elonmusk"
+          options={{ height: 400 }}
+        />
+
+        <TwitterShareButton
+          url={"https://npmahjong.com/"}
+          options={{ text: "#npmahjong", via: "_xkime" }}
+        />
+      </div>
     </div>
   );
 };
